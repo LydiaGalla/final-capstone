6c6e820491828956c75c57382a415ef510198fbf
John Sparks <johnrobertsparks@icloud.com>
2023-12-05 15:58:59 -0500

    database finalized

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -2,6 +2,14 @@ BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
 
+CREATE TABLE users (
+	user_id SERIAL,
+	username varchar(50) NOT NULL UNIQUE,
+	password_hash varchar(200) NOT NULL,
+	role varchar(50) NOT NULL,
+	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
 CREATE TABLE customer (
 	customer_id SERIAL,
 	firstname VARCHAR(25) NOT NULL,
@@ -21,13 +29,17 @@ CREATE TABLE employee (
     CONSTRAINT PK_employee PRIMARY KEY (employee_id)
 );
 
-CREATE TABLE standard_cake (
-    standard_cake_id SERIAL,
-    cake_name VARCHAR(25) NOT NULL UNIQUE,
-    description VARCHAR(500) NOT NULL,
-    price NUMERIC(8, 2) NOT NULL,
-    is_available BOOLEAN NOT NULL,
-    CONSTRAINT PK_standard_cake PRIMARY KEY (standard_cake_id)
+
+
+CREATE TABLE cake_price (
+    price_id SERIAL,
+    cupcake_single NUMERIC(6, 2) NOT NULL,
+    cupcake_bakers_dozen NUMERIC(6, 2) NOT NULL,
+    small_cake NUMERIC(6, 2) NOT NULL,
+    large_cake NUMERIC(6, 2) NOT NULL,
+    small_cake_custom NUMERIC(6, 2) NOT NULL,
+    large_cake_custom NUMERIC(6, 2) NOT NULL,
+    CONSTRAINT PK_cake_price PRIMARY KEY (price_id)
 );
 
 CREATE TABLE cake_flavors (
@@ -65,20 +77,32 @@ CREATE TABLE cake_style (
     CONSTRAINT PK_cake_style PRIMARY KEY (style_id)
 );
 
+CREATE TABLE standard_cake (
+    standard_cake_id SERIAL,
+    cake_name VARCHAR(25) NOT NULL UNIQUE,
+    description VARCHAR(500) NOT NULL,
+    price NUMERIC(6, 2) NOT NULL,
+    is_available BOOLEAN NOT NULL,
+    CONSTRAINT PK_standard_cake PRIMARY KEY (standard_cake_id)
+
+);
+
 CREATE TABLE custom_cake (
     custom_cake_id SERIAL,
+    cake_size_id INT,
     cake_flavor_id INT,
     cake_frosting_id INT,
     cake_filling_id INT,
-    cake_size_id INT,
     cake_style_id INT,
     extras VARCHAR(50) NOT NULL,
+    price_id INT NOT NULL,
     CONSTRAINT PK_custom_cake PRIMARY KEY (custom_cake_id),
     FOREIGN KEY (cake_flavor_id) REFERENCES cake_flavors(flavor_id),
     FOREIGN KEY (cake_frosting_id) REFERENCES cake_frostings(frosting_id),
     FOREIGN KEY (cake_filling_id) REFERENCES cake_fillings(filling_id),
     FOREIGN KEY (cake_size_id) REFERENCES cake_sizes(size_id),
-    FOREIGN KEY (cake_style_id) REFERENCES cake_style(style_id)
+    FOREIGN KEY (cake_style_id) REFERENCES cake_style(style_id),
+    FOREIGN KEY (price_id) REFERENCES cake_price(price_id)
 );
 
 CREATE TABLE cake_order (
@@ -91,11 +115,12 @@ CREATE TABLE cake_order (
     writing VARCHAR (140) NOT NULL,
     status VARCHAR (20) NOT NULL,
     quantity INTEGER NOT NULL,
+    total NUMERIC(8, 2) NOT NULL,
     CONSTRAINT PK_cake_order PRIMARY KEY (order_id),
     CONSTRAINT check_standard_custom_cake CHECK (
     (standard_cake_id IS NOT NULL AND custom_cake_id IS NULL) OR
     (standard_cake_id IS NULL AND custom_cake_id IS NOT NULL)
-    )
+    ),
     CONSTRAINT FK_cake_order_customer FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
 );
 

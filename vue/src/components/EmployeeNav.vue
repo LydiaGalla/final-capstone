5492b5ce02c575b6459c97ac66b62eb0e9023de0
Spencer Nicol <spncr.nicol@gmail.com>
2023-12-07 16:46:34 -0500

    Employee Home - Edit inventory

--- vue/src/components/EmployeeNav.vue
+++ vue/src/components/EmployeeNav.vue
@@ -3,7 +3,8 @@
         HI
         <!--TBD For Routerlink-->
         <router-link v-bind:to="{ name: 'employee home'}"> Orders </router-link> &nbsp;|&nbsp;
-        <router-link v-bind:to="{ name: 'inventory'}"> Inventory </router-link>
+        <router-link v-bind:to="{ name: 'inventory'}"> Inventory </router-link> &nbsp;|&nbsp;
+        <router-link v-bind:to="{ name: 'edit inventory'}"> Edit Inventory </router-link>
 
     </div>  
 

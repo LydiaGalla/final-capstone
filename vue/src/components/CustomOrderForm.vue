411a29bff15f455a8dccf292131e15e903e85377
Spencer Nicol <spncr.nicol@gmail.com>
2023-12-07 14:55:45 -0500

    Employee view, components, and setting up inventory editing

--- vue/src/components/CustomOrderForm.vue
+++ vue/src/components/CustomOrderForm.vue
@@ -10,53 +10,59 @@
 
             <label for="selectCakeFlavor">Select Cake Flavor</label>
             <select id="selectCakeFlavor" v-model="cakeFlavor">
-                <option>Vanilla</option>
-                <option>Chocolate</option>
-                <option>Funfetti</option>
+                <option value="vanilla">Vanilla</option>
+                <option value="chocolate">Chocolate</option>
+                <option value="funfetti">Funfetti</option>
             </select>
-
+            <br>
             <label for="selectIcingFlavor">Select Icing Flavor</label>
             <select id="selectIcingFlavor" v-model="icingFlavor">
-                <option>Vanilla</option>
-                <option>Chocolate</option>
-                <option>Buttercream</option>
+                <option value="vanilla">Vanilla</option>
+                <option value="chocolate">Chocolate</option>
+                <option value="buttercream">Buttercream</option>
             </select>
-
+            <br>
             <label for="selectStyle">Select Cake Style</label>
             <select id="selectStyle" v-model="style">
-                <option>Round</option>
-                <option>Sheet</option>
-                <option>Tiered</option>
+                <option value="round">Round</option>
+                <option value="sheet">Sheet</option>
+                <option value="tiered">Tiered</option>
             </select>
-
+            <br>
             <label for="selectSize">Select Cake Size</label>
             <select id="selectSize" v-model="size">
-                <option>Small</option>
-                <option>Large</option>
+                <option value="small">Small</option>
+                <option value="large">Large</option>
+                <option value="cupcake">Cupcakes</option>
             </select>
-
+            <br>
             <label for="selectFilling">Select Cake Filling</label>
             <select id="selectFilling" v-model="filling">
-                <option>Jam</option>
-                <option>Icing</option>
+                <option value="jam">Jam</option>
+                <option value="vanilla buttercream">Vanilla Buttercream</option>
+                <option value="fudge">Fudge</option>
+                <option value="custard">Custard</option>
             </select>
-
+            <br>
             <label for="customMessage">Add a Custom Message</label>
             <input type="text" id="customMessage">
             
-
+            <br>
             <!--customer section-->
             <h3>Customer Information</h3>
-    
+            <br>
             <label for="firstName">First Name:</label>
             <input type="text" id="firstName" v-model="firstName">
+            <br>
             <label for="lastName">Last Name:</label>
             <input type="text" id="lastName" v-model="lastName">
+            <br>
             <label for="phoneNumber">Phone Number: </label>
             <input type="text" id="phoneNumber" v-model="phoneNumber">
+            <br>
             <label for="email">E-mail: </label>
             <input type="text" id="email" v-model="email">
-
+            <br>
             <button type="submit" class="is-primary">Place Order</button>
 
         </form>
---
+++ vue/src/views/InventoryView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <StdCakesListVue></StdCakesListVue>
+
+
+    </div>
+
+
+
+</template>
+
+<script>
+import StdCakesListVue from '../components/StdCakesList.vue';
+
+export default{
+    data(){
+
+    },
+    components: {
+        StdCakesListVue
+    }
+
+}
+
+</script>

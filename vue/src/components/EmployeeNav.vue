82d9eeb0506f7273a10992d7f7bbc5aa7fa84e78
Lydia Gallagher <gallagherlydiae@gmail.com>
2023-12-07 16:47:56 -0500

    im not sure

--- vue/src/components/EmployeeNav.vue
+++ vue/src/components/EmployeeNav.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        HI
+        
         <!--TBD For Routerlink-->
         <router-link v-bind:to="{ name: 'employee home'}"> Orders </router-link> &nbsp;|&nbsp;
         <router-link v-bind:to="{ name: 'inventory'}"> Inventory </router-link>

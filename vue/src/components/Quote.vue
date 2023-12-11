863fc77a9814b85cf31a68f67021e0fd6e9d3559
Ryan Counasse <Rycounasse@outlook.com>
2023-12-11 14:20:49 -0500

    order confirm

---
+++ vue/src/components/Quote.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <header>
+      <p>{{ randomQuote }}</p>
+      <p>- {{ randomAuthor }}</p>
+        </header>
+    </div>
+  </template>
+  <script>
+  import axios from 'axios';
+  export default {
+    data() {
+      return {
+        randomQuote: '',
+      };
+    },
+    methods: {
+      fetchRandomQuote() {
+        const http = axios.create({ baseURL: 'https://api.api-ninjas.com/v1', headers: { 'X-Api-Key': 'nhJt+d9Aj9YoQbDr+ksKNA==lANgsP1MYhyxhO0f' }})
+        http.get('/quotes?category=inspirational')
+          .then(response => {
+            this.randomQuote = response.data[0].quote;
+            this.randomAuthor = response.data[0].author;
+          })
+          .catch(error => {
+            console.error('Error fetching random quote:', error);
+          });
+      },
+    },
+    created() {
+        this.fetchRandomQuote();
+    }
+  };
+  </script>
+
+<style scoped>
+
+div {
+  margin-top: 80px; 
+}
+</style>

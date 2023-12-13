ad04d390ea19b9d825f126447b92f0306faaa48f
Lydia Gallagher <gallagherlydiae@gmail.com>
2023-12-13 16:35:51 -0500

    working custom order cards for employees

--- vue/src/components/CustomOrderCard.vue
+++ vue/src/components/CustomOrderCard.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="custom-order-card">
-        <h2>Custom Order Information</h2>
+        <h2 v-if="!fromOrderCard">Custom Order Information</h2>
         <p class="custom-cake-flavor">Cake Flavor: {{ cake.cakeFlavor }}</p>
         <p class="custom-cake-frosting">Cake Frosting: {{ cake.cakeFrosting }}</p>
         <p class="custom-cake-filling">Cake Filling: {{ cake.cakeFilling }}</p>
         <p class="custom-cake-style">Style: {{ cake.cakeStyle }} </p>
         <p class="custom-cake-size">Size: {{ cake.cakeSize }} </p>
-        <p class="custom-cake-price">Price: $ {{ cake.cakePrice }}</p>
+        <p v-if="!fromOrderCard" class="custom-cake-price">Price: $ {{ cake.cakePrice }}</p>
     </div>
 </template>
 
@@ -15,12 +15,8 @@ import CustomOrderService from '../services/CustomOrderService';
 
     export default {
         props: {
+            fromOrderCard: Boolean,
             cake: Object,
-            data() {
-                return {
-                    cake: {}
-                }
-            }
         },
         methods: {
             
